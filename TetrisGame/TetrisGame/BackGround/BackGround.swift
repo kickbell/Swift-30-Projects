84067c3497c0e8c761a3d41d49cd5406c2595225
kickbell <dev.kickbell@gmail.com>
2021-11-30 00:32:32 +0900

    테트리스 백그라운드 벽 생성

--- TetrisGame/TetrisGame/BackGround/BackGround.swift
+++ TetrisGame/TetrisGame/BackGround/BackGround.swift
@@ -16,5 +16,25 @@ class BackGround {
   init() {
     //가로 10, 세로 20 크기의 2차원 배열 생성
     Variables.backarrays = Array(repeating: Array(repeating: 0, count: row), count: col)
+    bg()
   }
+  
+  //백그라운드 이중배열에서 각 테두리에 1을 넣어주는 작업
+  //즉, 1이 못움직이는거니까 벽을 만들어주는 작업이다.
+  func bg() {
+    for i in 0..<row {
+      Variables.backarrays[col-1][i] = 1
+    }
+    for i in 0..<col-1{
+      Variables.backarrays[i][0] = 1
+    }
+    for i in 0..<col-1 {
+      Variables.backarrays[i][row-1] = 1
+    }
+    for i in 0..<row {
+      Variables.backarrays[0][i] = 1
+    }
+  }
+  
+  
 }
