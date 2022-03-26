b117c10d5460facecf793c5cbc3d3476261c4574
kickbell <dev.kickbell@gmail.com>
2022-03-27 00:47:32 +0900

    add PhotoCell, ViewModel

---
+++ Project 03 - Prefetching TableView/PrefetchingTableView/PrefetchingTableView/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PrefetchingTableView
+//
+//  Created by jc.kim on 3/27/22.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCell: UITableViewCell {
+  
+}
---
+++ Project 03 - Prefetching TableView/PrefetchingTableView/PrefetchingTableView/ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModel.swift
+//  PrefetchingTableView
+//
+//  Created by jc.kim on 3/27/22.
+//
+
+import Foundation
+
+class ViewModel {
+  init() {
+    
+  }
+  
+  
+}
