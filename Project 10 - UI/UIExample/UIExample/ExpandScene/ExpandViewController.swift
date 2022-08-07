34145f7f457391d0d3e091ea4884982d70232c43
kickbell <dev.kickbell@gmail.com>
2022-08-07 18:30:14 +0900

    Add change BlackManager

--- Project 10 - UI/UIExample/UIExample/ExpandScene/ExpandViewController.swift
+++ Project 10 - UI/UIExample/UIExample/ExpandScene/ExpandViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
+struct BlockManager {
+    let blockUser: [User]
+    let nonBlolckUser: [User]
+}
+
+struct User: Identifiable {
+    let id: String
+    let profileImageUrl: String?
+    let chanelName: String
+    let isBlocked: Bool
+}
+
 class ExpandViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    let data = Array(1...30)
+    let data = BlockManager(
+        blockUser: [
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "이순신", isBlocked: true),
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "나대용", isBlocked: true),
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "장이수", isBlocked: true),
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "박개똥", isBlocked: true),
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "박거지", isBlocked: true),
+        ],
+        nonBlolckUser: [
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "나까무라", isBlocked: false),
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "와스히루", isBlocked: false),
+            User(id: UUID().uuidString, profileImageUrl: nil, chanelName: "히데요시", isBlocked: false)
+        ]
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +49,28 @@ class ExpandViewController: UIViewController {
 }
 
 extension ExpandViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "차단된 사용자" : "차단되지 않은 사용자"
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return section == 0 ? data.blockUser.count : data.nonBlolckUser.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "\(data[indexPath.row])"
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = data.blockUser[indexPath.row].chanelName
+        case 1:
+            cell.textLabel?.text = data.nonBlolckUser[indexPath.row].chanelName
+        default:
+            break
+        }
         return cell
     }
     
