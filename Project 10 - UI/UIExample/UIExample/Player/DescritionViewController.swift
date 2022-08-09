43fb345932d5421e24eef1b893c69372e0ff1985
kickbell <dev.kickbell@gmail.com>
2022-08-10 02:00:07 +0900

    Update MusicPlayerView

---
+++ Project 10 - UI/UIExample/UIExample/Player/DescritionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DescritionViewController.swift
+//  UIExample
+//
+//  Created by jc.kim on 8/9/22.
+//
+
+import UIKit
+
+class DescritionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Project 10 - UI/UIExample/UIExample/Player/PlayerMainViewController.swift
@@ -0,0 +1,154 @@
+//
+//  PlayerMainViewController.swift
+//  UIExample
+//
+//  Created by jc.kim on 8/9/22.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+    
+    @IBOutlet weak var topStackView: UIStackView!
+    
+    
+    @IBOutlet weak var titleStackView: UIStackView!
+    @IBOutlet weak var contentImageView: UIImageView!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
+    
+    @IBOutlet weak var bottomStackView: UIStackView!
+    
+    var callBack: (()->())?
+    
+    var callBackDescrition: (()->())?
+    
+    @IBAction func push(_ sender: Any) {
+        callBack?()
+    }
+    
+    @IBAction func presentDescrition(_ sender: Any) {
+        callBackDescrition?()
+    }
+}
+
+// FIXME: - 대충 어찌저찌된거같긴한데..
+//액션 이벤트 따로.
+//스와이프따로.
+//히든처리 따로 해서 구분을 해놓아야겠다.
+
+class PlayerMainViewController: UIViewController {
+    
+    @IBOutlet weak var miniPlayerVarView: PlayerView!
+    
+    @IBOutlet weak var heightLayoutConstraint: NSLayoutConstraint!
+    @IBOutlet weak var safeToTopLayoutConstraint: NSLayoutConstraint!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight))
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeUp))
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeDown))
+        swipeLeft.direction = .left
+        swipeRight.direction = .right
+        swipeUp.direction = .up
+        swipeDown.direction = .down
+        self.miniPlayerVarView.gestureRecognizers = [swipeUp, swipeDown, swipeLeft, swipeRight]
+        
+        
+        
+        
+        
+        
+        miniPlayerVarView.callBack = {
+            
+            let detailVC = DetailViewController()
+            self.navigationController?.pushViewController(detailVC, animated: true)
+        }
+        
+        miniPlayerVarView.callBackDescrition = {
+            let detailVC = DescritionViewController()
+            self.present(detailVC, animated: true)
+        }
+    }
+    
+    var isExpand = false
+    
+    @objc func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        print("swipeLeft...", sender.direction)
+    }
+    
+    @objc func swipeRight() {
+        print("swipeRight...")
+    }
+    
+    @objc func swipeUp() {
+        print("swipeUp...")
+        tap()
+    }
+    
+    @objc func swipeDown() {
+        print("swipeDown...")
+        tap()
+    }
+    
+    
+    @objc func tap() {
+        if !isExpand {
+            self.heightLayoutConstraint.priority = .defaultLow
+            self.safeToTopLayoutConstraint.priority = .defaultHigh
+        } else {
+            self.heightLayoutConstraint.priority = .defaultHigh
+            self.safeToTopLayoutConstraint.priority = .defaultLow
+        }
+        
+        
+        isExpand = !isExpand
+        
+        
+        
+        UIView.animate(withDuration: 0.3) {
+            self.miniPlayerVarView.topStackView.isHidden = !self.isExpand
+            self.miniPlayerVarView.titleStackView.isHidden = self.isExpand
+            self.miniPlayerVarView.button1.isHidden = self.isExpand
+            self.miniPlayerVarView.button2.isHidden = self.isExpand
+            self.miniPlayerVarView.bottomStackView.isHidden = !self.isExpand
+            
+            //top, bottom fade 효과
+            self.miniPlayerVarView.topStackView.alpha = !self.isExpand ? 0.0 : 1.0
+            self.miniPlayerVarView.bottomStackView.alpha = !self.isExpand ? 0.0 : 1.0
+
+            
+            
+            self.view.layoutIfNeeded()
+            
+        } completion: { b in
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
