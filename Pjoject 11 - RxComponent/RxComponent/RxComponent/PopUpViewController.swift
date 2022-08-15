2c39a016592f60f54e2cf3f9671a0ac846e6714c
kickbell <dev.kickbell@gmail.com>
2022-08-15 16:21:28 +0900

    Refactor Rx Logic

--- Pjoject 11 - RxComponent/RxComponent/RxComponent/PopUpViewController.swift
+++ Pjoject 11 - RxComponent/RxComponent/RxComponent/PopUpViewController.swift
@@ -40,71 +40,67 @@ class PopUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
         //month
         
-        oneMonthButton.rx.tap
-            .map { _ in Month.one }
-            .bind(to: monthSubject)
-            .disposed(by: disposeBag)
-        
-        threeMonthButton.rx.tap
-            .map { _ in Month.three }
-            .bind(to: monthSubject)
-            .disposed(by: disposeBag)
-        
-        sixMonthButton.rx.tap
-            .map { _ in Month.six }
-            .bind(to: monthSubject)
-            .disposed(by: disposeBag)
-        
-        monthSubject.map { $0 == .one }
-            .bind(to: oneMonthButton.rx.isSelected)
-            .disposed(by: disposeBag)
-        
-        monthSubject.map { $0 == .three }
-            .bind(to: threeMonthButton.rx.isSelected)
-            .disposed(by: disposeBag)
-        
-        monthSubject.map { $0 == .six }
-            .bind(to: sixMonthButton.rx.isSelected)
-            .disposed(by: disposeBag)
+        Observable.merge(
+            oneMonthButton.rx.tap.map { _ in Month.one },
+            threeMonthButton.rx.tap.map { _ in Month.three },
+            sixMonthButton.rx.tap.map { _ in Month.six }
+        )
+        .bind(to: monthSubject)
+        .disposed(by: disposeBag)
         
         
+        //위에타입은 같은데 아래가 다르다. 튜플
+        //두가지방법. 1. 머지 방법 또는
+        
+        Observable.merge(
+            monthSubject.map { ($0 == .one, self.oneMonthButton) },
+            monthSubject.map { ($0 == .three, self.threeMonthButton) },
+            monthSubject.map { ($0 == .six, self.sixMonthButton) }
+        )
+        .subscribe(onNext: { bool, button in
+            button.isSelected = bool
+        })
+        .disposed(by: disposeBag)
         
         //period
         
-        latestPeriodButton.rx.tap
-            .map { _ in Period.latest }
-            .bind(to: periodSubject)
-            .disposed(by: disposeBag)
+        Observable.merge(
+            latestPeriodButton.rx.tap.map { _ in Period.latest },
+            oldestPeriodButton.rx.tap.map { _ in Period.oldest }
+        )
+        .bind(to: periodSubject)
+        .disposed(by: disposeBag)
         
-        oldestPeriodButton.rx.tap
-            .map { _ in Period.oldest }
-            .bind(to: periodSubject)
-            .disposed(by: disposeBag)
+        //두가지방법. 2. 플랫맵 방법
+        periodSubject.flatMap {
+            Observable.from([
+                (bool: $0 == .latest, button: self.latestPeriodButton),
+                (bool: $0 == .oldest, button: self.oldestPeriodButton)
+            ])
+        }
+        .subscribe(onNext: { pair in
+            pair.button.isSelected = pair.bool
+        })
+        .disposed(by: disposeBag)
         
-        periodSubject.map { $0 == .latest }
-            .bind(to: latestPeriodButton.rx.isSelected)
-            .disposed(by: disposeBag)
-        
-        periodSubject.map { $0 == .oldest }
-            .bind(to: oldestPeriodButton.rx.isSelected)
-            .disposed(by: disposeBag)
-        
-        
-        
+//        periodSubject.map { $0 == .latest }
+//            .bind(to: latestPeriodButton.rx.isSelected)
+//            .disposed(by: disposeBag)
+//
+//        periodSubject.map { $0 == .oldest }
+//            .bind(to: oldestPeriodButton.rx.isSelected)
+//            .disposed(by: disposeBag)
         
         Observable.combineLatest (
             monthSubject.map { $0 != .none },
             periodSubject.map { $0 != .none }
         ) { $0 && $1 }
-        .bind(to: confirmButton.rx.isEnabled)
-        .disposed(by: disposeBag)
-        
-        
-        
+            .bind(to: confirmButton.rx.isEnabled)
+            .disposed(by: disposeBag)
     }
     
     
