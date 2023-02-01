f476a826b35dbcd971ff9257a213d51177cfa62a
kickbell <dev.kickbell@gmail.com>
2023-02-02 01:34:39 +0900

    add MediumTableCell

---
+++ Project 12 - AppStore Style/TapStore/MediumTableCell.swift
@@ -0,0 +1,59 @@
+//
+//  MediumTableCell.swift
+//  TapStore
+//
+//  Created by jc.kim on 2/2/23.
+//  Copyright © 2023 Hacking with Swift. All rights reserved.
+//
+
+import UIKit
+
+class MediumTableCell: UICollectionViewCell {
+    static var reuseIdentifier: String = "MediumTableCell"
+    
+    let name = UILabel()
+    let subtitle = UILabel()
+    let imageView = UIImageView()
+    let buyButton = UIButton(type: .custom)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        name.font = UIFont.preferredFont(forTextStyle: .headline)
+        name.textColor = .label
+         
+        subtitle.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        subtitle.textColor = .secondaryLabel
+        
+        imageView.layer.cornerRadius = 15
+        imageView.clipsToBounds = true
+        
+        buyButton.setImage(UIImage(systemName: "icloud.and.arrow.down"), for: .normal)
+        
+        let innerStackView = UIStackView(arrangedSubviews: [name, subtitle])
+        innerStackView.axis = .vertical
+        
+        let outerStackView = UIStackView(arrangedSubviews: [imageView, innerStackView, buyButton])
+        outerStackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(outerStackView)
+        
+        NSLayoutConstraint.activate([
+            outerStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            outerStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            outerStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            outerStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        //이것도 한번 정리해야됨.. 사용자 지정 이니셜라이저를 구현했기떄문에 얘가 필요한건데. 정확히 왜그런건지.
+        //정확히는 스토리보드에서 이 항목을 생성하는 것을 지원하지 않기위해 하는거다.
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with app: App) {
+        name.text = app.name
+        subtitle.text = app.subheading
+        imageView.image = UIImage(named: app.image)
+    }
+}
