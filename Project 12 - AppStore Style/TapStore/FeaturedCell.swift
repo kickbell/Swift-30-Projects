e8cdfd292b362deeda6d20c84d1dc7d00c28fd59
kickbell <dev.kickbell@gmail.com>
2023-02-01 23:33:37 +0900

    add featuredcell

--- Project 12 - AppStore Style/TapStore/FeaturedCell.swift
+++ Project 12 - AppStore Style/TapStore/FeaturedCell.swift
@@ -22,11 +22,41 @@ class FeaturedCell: UICollectionViewCell, SelfConfiguringCell {
         tagline.font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 12, weight: .bold))
         tagline.textColor = .systemBlue //라이트, 다크모드에 따라 달라지는 파랑색
         
+        name.font = UIFont.preferredFont(forTextStyle: .title2)
+        name.textColor = .label
+        
+        subtitle.font = UIFont.preferredFont(forTextStyle: .title2)
+        subtitle.textColor = .secondaryLabel
+        
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        
+        let stackView = UIStackView(arrangedSubviews: [tagline, name, subtitle, imageView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        stackView.setCustomSpacing(10, after: subtitle) //subtitle밑에만 10을 추가해주는건가본데..?
+    }
+    
+    required init?(coder: NSCoder) {
+        //이것도 한번 정리해야됨.. 사용자 지정 이니셜라이저를 구현했기떄문에 얘가 필요한건데. 정확히 왜그런건지.
+        //정확히는 스토리보드에서 이 항목을 생성하는 것을 지원하지 않기위해 하는거다.
+        fatalError("init(coder:) has not been implemented")
     }
     
     func configure(with app: App) {
-        
+        tagline.text = app.tagline.uppercased()
+        name.text = app.name
+        subtitle.text = app.subheading
+        imageView.image = UIImage(named: app.image)
     }
-    
-    
 }
