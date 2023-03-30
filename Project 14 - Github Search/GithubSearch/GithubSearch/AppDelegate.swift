bf498df7902d2ccc8429d8b1b985bc0c33659e6e
kickbell <dev.kickbell@gmail.com>
2023-03-30 17:29:55 +0900

    init

---
+++ Project 14 - Github Search/GithubSearch/GithubSearch/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  GithubSearch
+//
+//  Created by jc.kim on 3/30/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
---
+++ Project 14 - Github Search/GithubSearch/GithubSearch/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by jc.kim on 3/30/23.
+//
+
+import UIKit
+import ReactorKit
+import RxSwift
+
+
+class ViewController: UITableViewController {
+
+  let textfield = UITextField()
+  let button = UIButton()
+  let searchController = UISearchController(searchResultsController: nil)
+
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+
+
+}
+
