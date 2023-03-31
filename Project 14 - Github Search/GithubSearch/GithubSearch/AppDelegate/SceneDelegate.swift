932906d7a00ebc2b92e158e529c27ac20d7f2a52
kickbell <dev.kickbell@gmail.com>
2023-03-31 13:41:52 +0900

    update Delegate

--- Project 14 - Github Search/GithubSearch/GithubSearch/AppDelegate/SceneDelegate.swift
+++ Project 14 - Github Search/GithubSearch/GithubSearch/AppDelegate/SceneDelegate.swift
@@ -14,8 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     
+    let navigationController = UINavigationController(rootViewController: SearchViewController())
+    navigationController.navigationBar.prefersLargeTitles = true
+    
     window = UIWindow(windowScene: windowScene)
-    window?.rootViewController = ViewController()
+    window?.rootViewController = navigationController
     window?.makeKeyAndVisible()
   }
 
